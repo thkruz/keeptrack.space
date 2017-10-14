b447b0ee150b2f19ff1eb5ce79816c15176aeabe
Theodore Kruczek <theodore.kruczek@gmail.com>
2017-10-14 09:52:59 -0400

    Reorganized settingsManager.

--- js/settings-manager.js
+++ js/settings-manager.js
@@ -2,36 +2,30 @@
   var settingsManager = {};
 
   //  Version Control
-  settingsManager.versionNumber = 'v0.25.8';
+  settingsManager.versionNumber = 'v0.25.9';
   settingsManager.versionDate = 'October 12, 2017';
 
   settingsManager.shadersReady = false;
   settingsManager.cruncherReady = false;
 
-  // Search Variables
+  settingsManager.limitSats = '';
   settingsManager.searchLimit = 200;
 
-  // Map Variables
   settingsManager.mapWidth = 800;
   settingsManager.mapHeight = 600;
 
-  // ColorScheme settingsManager
+  settingsManager.currentColorScheme = null;
   settingsManager.otherSatelliteTransparency = 0.1;
 
   settingsManager.socratesOnSatCruncher = null;
 
   settingsManager.isSharperShaders = false;
-
   settingsManager.isEditTime = false;
   settingsManager.isPropRateChange = false;
-
   settingsManager.isOnlyFOVChecked = false;
   settingsManager.isBottomIconsEnabled = false;
   settingsManager.isBottomMenuOpen = false;
   settingsManager.isMapMenuOpen = false;
-
-  settingsManager.currentColorScheme = null;
-  settingsManager.limitSats = '';
 
   settingsManager.mapUpdateOverride = false;
   settingsManager.lastMapUpdateTime = 0;

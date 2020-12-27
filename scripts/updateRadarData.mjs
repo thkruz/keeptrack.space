e08bc9ff410ee031eed9017f821ee943f02df842
Theodore Kruczek <theodore.kruczek@gmail.com>
2020-12-27 00:09:04 -0500

    updateRadarData script

---
+++ scripts/updateRadarData.mjs
@@ -0,0 +1,20 @@
+import download from 'download-file';
+ 
+var urls = [
+    "https://www.keeptrack.space/radarData/radarData.txt",
+    "https://www.keeptrack.space/radarData/radarData.json"    
+];
+ 
+var options = {
+    directory: "./dist/radarData/",
+}
+ 
+urls.forEach(url => {
+    download(url, options, function(err) {
+        if (err) {
+            console.error(err);            
+        } else {
+            console.log(`${url} downloaded from KeepTrack.space`);
+        }
+    });
+});

d78687a85e15de91721af546cdc4dac13061bc7c
Theodore Kruczek <theodore.kruczek@gmail.com>
2020-12-27 19:35:23 -0500

    Fix Images Quick rename to fix images

--- src/offline/satInfo.js
+++ src/offline/satInfo.js
@@ -1,1 +1,1 @@
-// var satInfoList = [{"SCC":"11","ON":"VANGUARD 21","C":"US","LV":"U","LS":"AFETR","URL":"resources/images/00011.jpg"}]
+// var satInfoList = [{"SCC":"11","ON":"VANGUARD 21","C":"US","LV":"U","LS":"AFETR","URL":"resources/img/00011.jpg"}]

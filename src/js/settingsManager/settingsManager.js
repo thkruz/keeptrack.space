b2e44852a71ed0aa903442c5443f7f5972e97e87
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-04-28 15:10:47 -0400

    docs(settingsmanager): updated version number

--- src/js/settingsManager/settingsManager.js
+++ src/js/settingsManager/settingsManager.js
@@ -23,8 +23,8 @@ import { RADIUS_OF_EARTH } from '@app/js/lib/constants.js';
 let settingsManager = {};
 {
   //  Version Control
-  settingsManager.versionNumber = '3.0.7';
-  settingsManager.versionDate = 'April 5, 2021';
+  settingsManager.versionNumber = '3.0.8';
+  settingsManager.versionDate = 'April 28, 2021';
 
   // Install Folder Settings
   {

27d4c5850c55cec0ff3a7daaf6e8e31e677d1a8c
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-05-11 22:36:47 -0400

    test(startup.js): simplified cypress test

--- cypress/integration/startup.js
+++ cypress/integration/startup.js
@@ -5,25 +5,9 @@ context('KeepTrack Startup', () => {
     cy.visit('http://127.0.0.1:8080/index.htm');
   });
 
-  it('Gremlins', () => {
-    cy.window().then((win) => {
-      cy.wait(30000); // wait 10 seconds
-      cy.on('uncaught:exception', (err, runnable) => {
-        expect(err.message).to.include('something about the error');
-        return false;
-      });
-      win.db.gremlins();
-    });
-  });
-
   it('Load Site', () => {
     cy.window().then((win) => {
-      cy.wait(60000); // wait 10 seconds
-      cy.on('uncaught:exception', (err, runnable) => {
-        expect(err.message).to.include('something about the error');
-        return false;
-      });
-      cy.get('#menu-time-machine').trigger('click'); // Trigger mousedown event on link
+      cy.wait(60000); // wait 60 seconds
     });
   });
 });

94ae5ef24aed1117ad09a91b2703ca091824906d
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-05-11 22:05:49 -0400

    test(startup.js): increase wait to 10s

--- cypress/integration/startup.js
+++ cypress/integration/startup.js
@@ -7,14 +7,14 @@ context('KeepTrack Startup', () => {
 
   it('Gremlins', () => {
     cy.window().then((win) => {
-      cy.wait(3500); // wait 3.5 seconds
+      cy.wait(10000); // wait 10 seconds
       win.db.gremlins();
     });
   });
 
   it('Load Site', () => {
     cy.window().then((win) => {
-      cy.wait(3500); // wait 3.5 seconds
+      cy.wait(10000); // wait 10 seconds
       cy.get('#menu-time-machine').trigger('click'); // Trigger mousedown event on link
     });
   });

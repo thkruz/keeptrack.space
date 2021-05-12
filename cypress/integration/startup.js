deeec91a9570a67f4b67df4ec8cecb950d5d45a8
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-05-11 22:22:29 -0400

    test(startup.js): added error handling

--- cypress/integration/startup.js
+++ cypress/integration/startup.js
@@ -2,19 +2,27 @@
 
 context('KeepTrack Startup', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:8080/index.htm');
+    cy.visit('http://127.0.0.1:8080/index.htm');
   });
 
   it('Gremlins', () => {
     cy.window().then((win) => {
-      cy.wait(10000); // wait 10 seconds
+      cy.wait(30000); // wait 10 seconds
+      cy.on('uncaught:exception', (err, runnable) => {
+        expect(err.message).to.include('something about the error');
+        return false;
+      });
       win.db.gremlins();
     });
   });
 
   it('Load Site', () => {
     cy.window().then((win) => {
-      cy.wait(30000); // wait 10 seconds
+      cy.wait(60000); // wait 10 seconds
+      cy.on('uncaught:exception', (err, runnable) => {
+        expect(err.message).to.include('something about the error');
+        return false;
+      });
       cy.get('#menu-time-machine').trigger('click'); // Trigger mousedown event on link
     });
   });

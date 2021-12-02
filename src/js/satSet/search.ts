64f26c3d493e242ab457937210b4cc10c7437d8b
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-12-01 19:27:54 -0500

    refactor(search.ts): shorter filtering

--- src/js/satSet/search.ts
+++ src/js/satSet/search.ts
@@ -1,48 +1,32 @@
+import { SatObject } from '../api/keepTrack';
+
 export const searchYear = (satData: any, year: any) => {
-  const res = [];
-  for (let i = 0; i < satData.length; i++) {
-    if (typeof satData[i].TLE1 == 'undefined')
-      continue;
-    if (satData[i].TLE1.substring(9, 11) == year) {
-      res.push(i);
-    }
-  }
-  return res;
+  const result = satData.filter((sat: SatObject) => {
+    const tleYear = sat?.TLE1?.substring(9, 11) || '-1';
+    return !!(parseInt(tleYear) == year);
+  });
+  return result;
 };
+
 export const searchYearOrLess = (satData: any, year: number) => {
-  const res = [];
-  for (let i = 0; i < satData.length; i++) {
-    if (typeof satData[i].TLE1 == 'undefined')
-      continue;
+  const result = satData.filter((sat: SatObject) => {
+    const tleYear = sat?.TLE1?.substring(9, 11) || '-1';
     if (year >= 59 && year < 100) {
-      if (satData[i].TLE1.substring(9, 11) <= year && satData[i].TLE1.substring(9, 11) >= 59) {
-        res.push(i);
-      }
+      return !!(parseInt(tleYear) <= year && parseInt(tleYear) >= 59);
     } else {
-      if (satData[i].TLE1.substring(9, 11) <= year || satData[i].TLE1.substring(9, 11) >= 59) {
-        res.push(i);
-      }
+      return !!(parseInt(tleYear) <= year || parseInt(tleYear) >= 59);
     }
-  }
-  return res;
+  });
+  return result;
+};
+
+// eslint-disable-next-line no-unused-vars
+export const searchNameRegex = (satData: any, regex: { test: (arg0: any) => any }) => {
+  const result = satData.filter((sat: SatObject) => !!regex.test(sat.ON));
+  return result;
 };
 // eslint-disable-next-line no-unused-vars
-export const searchNameRegex = (satData: any, regex: { test: (arg0: any) => any; }) => {
-  const res = [];
-  for (let i = 0; i < satData.length; i++) {
-    if (regex.test(satData[i].ON)) {
-      res.push(i);
-    }
-  }
-  return res;
-};
-// eslint-disable-next-line no-unused-vars
-export const searchCountryRegex = (satData: any, regex: { test: (arg0: any) => any; }) => {
-  const res = [];
-  for (let i = 0; i < satData.length; i++) {
-    if (regex.test(satData[i].C)) {
-      res.push(i);
-    }
-  }
-  return res;
+export const searchCountryRegex = (satData: any, regex: { test: (arg0: any) => any }) => {
+  const result = satData.filter((sat: SatObject) => !!regex.test(sat.C));
+  return result;
 };

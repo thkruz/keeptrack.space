2c1e16b7627f355f7797bb5d568b8f4d8e222ca9
Theodore Kruczek <theodore.kruczek@gmail.com>
2021-12-11 09:42:24 -0500

    test(getorbitbylatlon): created tests for getorbitbylatlon

---
+++ src/js/satMath/getOrbitByLatLon.test.ts
@@ -0,0 +1,41 @@
+import { defaultSat } from '../api/apiMocks';
+import * as getOrbitByLatLon from './getOrbitByLatLon';
+
+// @ponicode
+describe('getOrbitByLatLon.getOrbitByLatLon', () => {
+  test('0', () => {
+    let param5: any = new Date('01-01-2020');
+    let result: any = getOrbitByLatLon.getOrbitByLatLon(defaultSat, 0.15, -0.85, 'S', param5);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('1', () => {
+    let param5: any = new Date('01-01-2020');
+    let result: any = getOrbitByLatLon.getOrbitByLatLon(defaultSat, 11.0, 11, 'N', param5);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('2', () => {
+    let param5: any = new Date('01-01-2020');
+    let result: any = getOrbitByLatLon.getOrbitByLatLon(defaultSat, 0.15, 11.0, 'S', param5);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('3', () => {
+    let param5: any = new Date('01-01-2020');
+    let result: any = getOrbitByLatLon.getOrbitByLatLon(defaultSat, 11, 11, 'S', param5, 100, -1.0);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('4', () => {
+    let param5: any = new Date('32-01-2020');
+    let result: any = getOrbitByLatLon.getOrbitByLatLon(defaultSat, 11.0, 12, 'S', param5, 100, 0.0);
+    expect(result).toMatchSnapshot();
+  });
+
+  test('5', () => {
+    let param5: any = new Date('');
+    let result: any = () => getOrbitByLatLon.getOrbitByLatLon(defaultSat, Infinity, Infinity, 'S', param5, Infinity, Infinity);
+    expect(result).toMatchSnapshot();
+  });
+});

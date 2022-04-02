46a631701a8e1a1840781a0d6a437c80f0e8168c
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-04-02 10:05:34 -0400

    feat: :sparkles: add ecf analysis plot

--- src/js/satMath/satMath.ts
+++ src/js/satMath/satMath.ts
@@ -1527,6 +1527,20 @@ export const getEciOfCurrentObit = (sat: SatObject, points: number): { x: number
   return eciPoints;
 }
 
+export const getEcfOfCurrentObit = (sat: SatObject, points: number): { x: number; y: number; z: number }[] => {
+  const { timeManager } = keepTrackApi.programs;
+
+  // Set default timing settings. These will be changed to find look angles at different times in future.
+  const simulationTime = timeManager.calculateSimulationTime();
+  let ecfPoints = [];
+  for (let i = 0; i < points; i++) {
+    let offset = ((i * sat.period) / points) * 60 * 1000; // Offset in seconds (msec * 1000)
+    const now = timeManager.getOffsetTimeObj(offset, simulationTime);
+    ecfPoints.push(satellite.ecfToEci(getEci(sat, now).position, -i * (sat.period / points) * TAU / sat.period));
+  }
+  return ecfPoints;
+}
+
 export const calculateSensorPos = (sensors?: SensorObject[]): { x: number; y: number; z: number; lat: number; lon: number; gmst: number } => {
   const { timeManager, sensorManager } = keepTrackApi.programs;
   sensors = verifySensors(sensors, sensorManager);
@@ -1684,6 +1698,7 @@ export const satellite: SatMath = {
   getDops,
   getEci,
   getEciOfCurrentObit,
+  getEcfOfCurrentObit,
   getlookangles,
   getlookanglesMultiSite,
   getOrbitByLatLon,

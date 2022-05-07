d9930bc1feeb58c24afdd8838dcf6c60c016ff0f
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-05-07 14:41:04 -0400

    fix: :beers: start fixing trend analysis menu

--- scripts/lib/setupFolders.mjs
+++ scripts/lib/setupFolders.mjs
@@ -2,7 +2,22 @@ import mkdirp from 'mkdirp';
 import rimraf from 'rimraf';
 
 export const setupDistFolders = () => {
-  rimraf.sync('./dist', { recursive: true });
+  rimraf.sync('./dist/admin', { recursive: true });
+  rimraf.sync('./dist/audio', { recursive: true });
+  rimraf.sync('./dist/css', { recursive: true });
+  rimraf.sync('./dist/img', { recursive: true });
+  rimraf.sync('./dist/js', { recursive: true });
+  rimraf.sync('./dist/meshes', { recursive: true });
+  rimraf.sync('./dist/offline', { recursive: true });
+  rimraf.sync('./dist/php', { recursive: true });
+  rimraf.sync('./dist/plugins', { recursive: true });
+  rimraf.sync('./dist/radarData', { recursive: true });
+  rimraf.sync('./dist/res', { recursive: true });
+  rimraf.sync('./dist/settings', { recursive: true });
+  rimraf.sync('./dist/simulation', { recursive: true });
+  rimraf.sync('./dist/textures', { recursive: true });
+  rimraf.sync('./dist/tle', { recursive: true });
+  rimraf.sync('./dist/*', { recursive: false });
   mkdirp.sync('./dist');
 
   mkdirp.sync('./dist/plugins');

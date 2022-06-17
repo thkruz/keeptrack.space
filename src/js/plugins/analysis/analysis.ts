fd66305b7a4de10e4124f1b2b9df6c632ff1ca0d
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-06-17 10:09:58 -0400

    docs: :page_facing_up: clear AGPL notice

--- src/js/plugins/analysis/analysis.ts
+++ src/js/plugins/analysis/analysis.ts
@@ -4,8 +4,9 @@ import { clickAndDragWidth, getEl, showLoading, slideInRight, slideOutLeft } fro
 import { AnalysisBottomIcon } from './components/AnalysisBottomIcon';
 import { AnalysisSideMenu } from './components/AnalysisSideMenu';
 import { OrbitOptionGroup, RaeOptionGroup } from './components/TrendAnalysis';
+
 /**
- * /////////////////////////////////////////////////////////////////////////////
+ * /*! /////////////////////////////////////////////////////////////////////////////
  *
  * analysis.ts is a plugin for viewing trend data on TLEs and calculating best
  * pass times.
@@ -13,15 +14,17 @@ import { OrbitOptionGroup, RaeOptionGroup } from './components/TrendAnalysis';
  * http://keeptrack.space
  *
  * @Copyright (C) 2016-2022 Theodore Kruczek
+ * @Copyright (C) 2020-2022 Heather Kruczek
  *
- * KeepTrack is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
+ * KeepTrack is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * KeepTrack is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with
+ * KeepTrack is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
  * KeepTrack. If not, see <http://www.gnu.org/licenses/>.
  *
  * /////////////////////////////////////////////////////////////////////////////

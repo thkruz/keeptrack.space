47d6ca20c832d3ff61232f80254566db90dea6cc
Theodore Kruczek <theodore.kruczek@gmail.com>
2022-11-11 17:11:22 -0500

    feat: :sparkles: add help menu to colors menu

---
+++ src/js/plugins/colors-menu/help.ts
@@ -0,0 +1,8 @@
+import { keepTrackApi } from '@app/js/api/keepTrackApi';
+
+export const helpTitleTextColor = `Colors Menu`;
+
+export const helpBodyTextColor = keepTrackApi.html`The Colors Menu is a place to change the color theme used to render the objects.
+<br><br>
+The various themes can change the colors based on the objects' orbits, objects' characteristics, or the objects' relation to sun and/or earth.
+`;

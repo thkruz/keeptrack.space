b8c412775a4dd664c2e7c8e90fc98bb86592531b
Theodore Kruczek <theodore.kruczek@gmail.com>
2023-01-10 21:31:53 -0500

    fix: :bug: add error logging for possible bug #635

--- src/js/satMath/calc/getAlt.ts
+++ src/js/satMath/calc/getAlt.ts
@@ -1,9 +1,15 @@
-import { EciVec3, Sgp4 } from 'ootk';
+import { EciVec3, SatelliteRecord, Sgp4 } from 'ootk';
 import { eci2lla } from '../transforms';
 import { calculateTimeVariables } from './calculateTimeVariables';
 
 export const getAlt = (tle1: string, tle2: string, now: Date) => {
-  const satrec = Sgp4.createSatrec(tle1, tle2); // perform and store sat init calcs
+  let satrec: SatelliteRecord;
+  try {
+    satrec = Sgp4.createSatrec(tle1, tle2); // perform and store sat init calcs
+  } catch (e) {
+    console.error('Error creating satrec for', tle1, tle2, e);
+    return 0;
+  }
   const { m, gmst } = calculateTimeVariables(now, satrec);
   let positionEci = <EciVec3>Sgp4.propagate(satrec, m).position;
   if (!positionEci) {

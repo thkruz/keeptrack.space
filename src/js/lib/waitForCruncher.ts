43f6006fc1796d4506741efd68d6e8749a573792
Theodore Kruczek <theodore.kruczek@gmail.com>
2023-11-24 07:33:58 -0500

    fix: :bug: add error checking to new launch plugin

--- src/js/lib/waitForCruncher.ts
+++ src/js/lib/waitForCruncher.ts
@@ -1,26 +1,31 @@
 import { errorManagerInstance } from '../singletons/errorManager';
 
+const DEFAULT_ERROR = () => {
+  errorManagerInstance.info('Cruncher failed to meet requirement after two tries!');
+};
 
-export const waitForCruncher = (cruncher: Worker, cb: () => void, validationFunc: (data: any) => boolean): void => {
-    cruncher.addEventListener(
-        'message',
-        (m) => {
+export const waitForCruncher = (cruncher: Worker, cb: () => void, validationFunc: (data: any) => boolean, error = DEFAULT_ERROR): void => {
+  cruncher.addEventListener(
+    'message',
+    (m) => {
+      if (validationFunc(m.data)) {
+        cb();
+      } else {
+        console.warn(m.data);
+        cruncher.addEventListener(
+          'message',
+          (m) => {
             if (validationFunc(m.data)) {
-                cb();
+              cb();
             } else {
-                cruncher.addEventListener(
-                    'message',
-                    (m) => {
-                        if (validationFunc(m.data)) {
-                            cb();
-                        } else {
-                            errorManagerInstance.info('Cruncher failed to meet requirement after two tries!');
-                        }
-                    },
-                    { once: true }
-                );
+              console.warn(m.data);
+              error();
             }
-        },
-        { once: true }
-    );
+          },
+          { once: true }
+        );
+      }
+    },
+    { once: true }
+  );
 };

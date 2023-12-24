d1e10f679a021871122345c01a6dcce1e8b21699
Theodore Kruczek <theodore.kruczek@gmail.com>
2023-12-24 13:08:40 -0500

    feat: :sparkles: update socrates to json input

--- src/plugins/collisions/collisions.ts
+++ src/plugins/collisions/collisions.ts
@@ -3,25 +3,37 @@ import collissionsPng from '@public/img/icons/socrates.png';
 
 import { getEl } from '@app/lib/get-el';
 import { showLoading } from '@app/lib/showLoading';
-import { keepTrackApi } from '../../keepTrackApi';
+import { keepTrackApi, KeepTrackApiEvents } from '../../keepTrackApi';
 import { clickDragOptions, KeepTrackPlugin } from '../KeepTrackPlugin';
 import { SelectSatManager } from '../select-sat-manager/select-sat-manager';
 
 interface CollisionEvent {
   sat1: string;
+  sat1Name: string;
+  sat1Status: string;
   sat2: string;
-  toca: Date;
+  sat2Name: string;
+  sat2Status: string;
+  /** Number of Days */
+  sat1AgeOfTLE: number;
+  /** Number of Days */
+  sat2AgeOfTLE: number;
+  /** ISO Date format */
+  toca: string;
   minRng: number;
-  maxProb: number;
+  dilutionThreshold: number;
   relSpeed: number;
+  maxProb: number;
 }
 
 export class CollissionsPlugin extends KeepTrackPlugin {
   static PLUGIN_NAME = 'collisions';
+
   constructor() {
     super(CollissionsPlugin.PLUGIN_NAME);
   }
 
+  private readonly collisionDataSrc = './tle/SOCRATES.json';
   bottomIconElementName: string = 'menu-satellite-collision';
   bottomIconImg = collissionsPng;
   bottomIconLabel: string = 'Collisions';
@@ -45,8 +57,8 @@ export class CollissionsPlugin extends KeepTrackPlugin {
   collisionList = <CollisionEvent[]>[];
   dragOptions: clickDragOptions = {
     isDraggable: true,
-    minWidth: 350,
-    maxWidth: 500,
+    minWidth: 500,
+    maxWidth: 650,
   };
 
   bottomIconCallback: () => void = () => {
@@ -56,14 +68,14 @@ export class CollissionsPlugin extends KeepTrackPlugin {
   };
 
   private uiManagerFinal() {
-    getEl('socrates-menu').addEventListener('click', (evt: any) => {
+    getEl(this.sideMenuElementName).addEventListener('click', (evt: any) => {
       showLoading(() => {
         const el = <HTMLElement>evt.target.parentElement;
         if (!el.classList.contains('socrates-object')) return;
         // Might be better code for this.
-        const hiddenRow = (<any>el.attributes).hiddenrow.value;
+        const hiddenRow = el.dataset?.row;
         if (hiddenRow !== null) {
-          this.eventClicked_(hiddenRow);
+          this.eventClicked_(parseInt(hiddenRow));
         }
       });
     });
@@ -73,14 +85,14 @@ export class CollissionsPlugin extends KeepTrackPlugin {
     super.addJs();
 
     keepTrackApi.register({
-      event: 'uiManagerFinal',
-      cbName: 'collisions',
+      event: KeepTrackApiEvents.uiManagerFinal,
+      cbName: this.PLUGIN_NAME,
       cb: this.uiManagerFinal.bind(this),
     });
 
     keepTrackApi.register({
-      event: 'onCruncherMessage',
-      cbName: 'collisions',
+      event: KeepTrackApiEvents.onCruncherMessage,
+      cbName: this.PLUGIN_NAME,
       cb: () => {
         if (this.socratesOnSatCruncher !== null) {
           // If selectedSatManager is loaded, set the selected sat to the one that was just added
@@ -95,11 +107,10 @@ export class CollissionsPlugin extends KeepTrackPlugin {
   private parseCollisionData_() {
     if (this.collisionList.length === 0) {
       // Only generate the table if receiving the -1 argument for the first time
-      fetch('./SOCRATES.html').then((response) => {
-        response.text().then((text) => {
-          const parser = new DOMParser();
-          const socratesHTM = parser.parseFromString(text, 'text/html');
-          this.processSocratesHtm(socratesHTM);
+      fetch(this.collisionDataSrc).then((response) => {
+        response.json().then((collisionList: CollisionEvent[]) => {
+          this.collisionList = collisionList;
+          this.createTable_();
 
           if (this.collisionList.length === 0) {
             errorManagerInstance.warn('No collisions data found!');
@@ -111,7 +122,7 @@ export class CollissionsPlugin extends KeepTrackPlugin {
 
   private eventClicked_(row: number) {
     const now = new Date();
-    keepTrackApi.getTimeManager().changeStaticOffset(this.collisionList[row].toca.getTime() - now.getTime() - 1000 * 30);
+    keepTrackApi.getTimeManager().changeStaticOffset(new Date(this.collisionList[row].toca).getTime() - now.getTime() - 1000 * 30);
     keepTrackApi.getMainCamera().isAutoPitchYawToTarget = false;
 
     const sat1 = this.collisionList[row].sat1.padStart(5, '0');
@@ -119,66 +130,57 @@ export class CollissionsPlugin extends KeepTrackPlugin {
 
     keepTrackApi.getUiManager().doSearch(`${sat1},${sat2}`);
     const catalogManagerInstance = keepTrackApi.getCatalogManager();
-    this.socratesOnSatCruncher = catalogManagerInstance.getIdFromObjNum(parseInt(sat1));
+    this.socratesOnSatCruncher = catalogManagerInstance.getIdFromSccNum(parseInt(sat1));
   }
 
-  private processSocratesHtm(socratesHTM: Document): void {
-    // TODO: This should be pushed to the daily cron job!
+  private createTable_(): void {
     try {
-      // Find a table whose class is "center outline" using pure javascript
-      const table = socratesHTM.getElementsByClassName('center outline')[0];
-      // Filter out all tr elements that have a class of "header" using pure javascript
-      const tableRows = Array.from(table.getElementsByTagName('tr')).filter((tr) => !tr.classList.contains('header'));
-
-      // Split the td elements into an array of arrays using pure javascript
-      const tableData = tableRows.map((tr) => Array.from(tr.getElementsByTagName('td'))).map((tds) => tds.map((td) => td.innerText));
-
-      for (let row = 0; row < tableData.length; row = row + 2) {
-        const event: CollisionEvent = {
-          sat1: tableData[row][1],
-          sat2: tableData[row + 1][1],
-          toca: new Date(tableData[row][4]),
-          minRng: parseFloat(tableData[row][5]),
-          maxProb: parseFloat(tableData[row + 1][5]),
-          relSpeed: parseFloat(tableData[row][6]),
-        };
-        this.collisionList.push(event);
-      }
-
-      // SOCRATES Menu
       const tbl = <HTMLTableElement>getEl('socrates-table'); // Identify the table to update
       tbl.innerHTML = ''; // Clear the table from old object data
-      let tr = tbl.insertRow();
-      let tdToca = tr.insertCell();
-      tdToca.appendChild(document.createTextNode('TOCA'));
-      tdToca.setAttribute('style', 'text-decoration: underline');
-      let tdSat1 = tr.insertCell();
-      tdSat1.appendChild(document.createTextNode('#1'));
-      tdSat1.setAttribute('style', 'text-decoration: underline');
-      let tdSat2 = tr.insertCell();
-      tdSat2.appendChild(document.createTextNode('#2'));
-      tdSat2.setAttribute('style', 'text-decoration: underline');
-      let tdDist = tr.insertCell();
-      tdDist.appendChild(document.createTextNode('Probability'));
-      tdDist.setAttribute('style', 'text-decoration: underline');
 
-      for (let i = 0; i < this.collisionList.length; i++) {
-        tr = tbl.insertRow();
-        tr.setAttribute('class', 'socrates-object link');
-        tr.setAttribute('hiddenrow', i.toString());
-        tdToca = tr.insertCell();
-        // Add the time in the format "YYYY-MM-DD HH:MM:SS"
-        tdToca.appendChild(document.createTextNode(this.collisionList[i].toca.toISOString().slice(0, 19).replace('T', ' ')));
-        tdSat1 = tr.insertCell();
-        tdSat1.appendChild(document.createTextNode(this.collisionList[i].sat1));
-        tdSat2 = tr.insertCell();
-        tdSat2.appendChild(document.createTextNode(this.collisionList[i].sat2));
-        tdDist = tr.insertCell();
-        tdDist.appendChild(document.createTextNode(this.collisionList[i].minRng.toString()));
-      }
+      CollissionsPlugin.createHeaders_(tbl);
+
+      this.createBody_(tbl);
     } catch (e) {
-      errorManagerInstance.warn('Error parsing SOCRATES data!');
+      errorManagerInstance.warn('Error processing SOCRATES data!');
     }
+  }
+
+  private createBody_(tbl: HTMLTableElement) {
+    for (let i = 0; i < this.collisionList.length; i++) {
+      this.createRow_(tbl, i);
+    }
+  }
+
+  private static createHeaders_(tbl: HTMLTableElement) {
+    let tr = tbl.insertRow();
+    const names = ['TOCA', '#1', '#2', 'Max Prob', 'Min Range (km)', 'Rel Speed (km/s)'];
+    for (const name of names) {
+      const column = tr.insertCell();
+      column.appendChild(document.createTextNode(name));
+      column.setAttribute('style', 'text-decoration: underline');
+    }
+  }
+
+  private createRow_(tbl: HTMLTableElement, i: number): HTMLTableRowElement {
+    // Create a new row
+    const tr = tbl.insertRow();
+    tr.setAttribute('class', 'socrates-object link');
+    tr.setAttribute('data-row', i.toString());
+
+    // Populate the table with the data
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].toca.slice(0, 19).replace('T', ' '));
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].sat1);
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].sat2);
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].maxProb.toString());
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].minRng.toString());
+    CollissionsPlugin.createCell_(tr, this.collisionList[i].relSpeed.toString());
+    return tr;
+  }
+
+  private static createCell_(tr: HTMLTableRowElement, text: string): void {
+    const cell = tr.insertCell();
+    cell.appendChild(document.createTextNode(text));
   }
 }
 

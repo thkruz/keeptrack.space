01323967307abd967944c5ecb27915a84f0418c8
Theodore Kruczek <theodore.kruczek@gmail.com>
2023-12-27 14:45:13 -0500

    fix: :bug: add callbacks for sensor dot selected

--- src/plugins/sensor-fov/sensor-fov.ts
+++ src/plugins/sensor-fov/sensor-fov.ts
@@ -78,6 +78,22 @@ export class SensorFov extends KeepTrackPlugin {
     });
 
     keepTrackApi.register({
+      event: KeepTrackApiEvents.sensorDotSelected,
+      cbName: this.PLUGIN_NAME,
+      cb: (sensor: SensorObject): void => {
+        if (sensor) {
+          getEl(this.bottomIconElementName).classList.remove(KeepTrackPlugin.iconDisabledClassString);
+          this.isIconDisabled = false;
+        } else {
+          getEl(this.bottomIconElementName).classList.add(KeepTrackPlugin.iconDisabledClassString);
+          this.isIconDisabled = true;
+          this.isMenuButtonActive = false;
+          getEl(this.bottomIconElementName).classList.remove(KeepTrackPlugin.iconSelectedClassString);
+        }
+      },
+    });
+
+    keepTrackApi.register({
       event: KeepTrackApiEvents.changeSensorMarkers,
       cbName: this.PLUGIN_NAME,
       cb: (caller: string): void => {

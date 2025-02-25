97d98e1169431274c5fd70675873966a05e7f35a
Norbert Pouzin <npouzin@aldoria.com>
2025-02-25 13:59:13 +0100

    🎚️  Propagation slider added to the calendar plugin

--- src/plugins/date-time-manager/calendar.ts
+++ src/plugins/date-time-manager/calendar.ts
@@ -8,6 +8,7 @@ export class Calendar {
   private containerId: string;
   private calendarDate: Date;
   private simulationDate: Date | null;
+  private propagationRate: number = 1;
   private isVisible: boolean = false;
   private timerUntilEnabled: number | null = 10000;
   private isCalendarEnabled: boolean = false;
@@ -72,6 +73,9 @@ export class Calendar {
         <button type="button" class="ui-datepicker-current ui-state-default ui-priority-secondary ui-corner-all" data-handler="today" data-event="click">
           ${i18next.t('time.calendar.now')}
         </button>
+        <button type="button" class="ui-datepicker-pause ui-state-default ui-priority-secondary ui-corner-all" data-handler="pause" data-event="click">
+          ${i18next.t('Pause')}
+        </button>
         <button type="button" class="ui-datepicker-close ui-state-default ui-priority-primary ui-corner-all" data-handler="hide" data-event="click">
           ${i18next.t('time.calendar.done')}
         </button>
@@ -100,12 +104,19 @@ export class Calendar {
     });
 
     const nowButton = document.querySelector('#ui-datepicker-div .ui-datepicker-current');
+    const pauseButton = document.querySelector('#ui-datepicker-div .ui-datepicker-pause');
     const doneButton = document.querySelector('#ui-datepicker-div .ui-datepicker-close');
 
     if (nowButton) {
       nowButton.addEventListener('click', this.setToNow.bind(this));
     }
-
+    if (pauseButton) {
+      if (this.propagationRate == 0) {
+        pauseButton.addEventListener('click', this.playProp.bind(this));
+      } else {
+        pauseButton.addEventListener('click', this.pauseProp.bind(this));
+      }
+    }
     if (doneButton) {
       doneButton.addEventListener('click', this.hideDatePicker.bind(this));
     }
@@ -114,14 +125,16 @@ export class Calendar {
     this.attachSliderEvents('ui_tpicker_hour_slider', this.updateHour.bind(this));
     this.attachSliderEvents('ui_tpicker_minute_slider', this.updateMinute.bind(this));
     this.attachSliderEvents('ui_tpicker_second_slider', this.updateSecond.bind(this));
+    this.attachSliderEvents('ui_tpicker_proprate_slider', this.updatePropRate.bind(this));
 
     // Attach events for time adjustment buttons
     this.attachTimeAdjustmentEvents('hour');
     this.attachTimeAdjustmentEvents('minute');
     this.attachTimeAdjustmentEvents('second');
+    this.attachTimeAdjustmentEvents('rate');
   }
 
-  private attachTimeAdjustmentEvents(unit: 'hour' | 'minute' | 'second'): void {
+  private attachTimeAdjustmentEvents(unit: 'hour' | 'minute' | 'second' | 'rate'): void {
     const increaseButton = document.getElementById(`${unit}_increase`);
     const decreaseButton = document.getElementById(`${unit}_decrease`);
 
@@ -131,7 +144,7 @@ export class Calendar {
     }
   }
 
-  private adjustTime(unit: 'hour' | 'minute' | 'second', change: 1 | -1): void {
+  private adjustTime(unit: 'hour' | 'minute' | 'second' | 'rate', change: 1 | -1): void {
     let currentValue: number;
 
     if (unit === 'hour') {
@@ -179,8 +192,16 @@ export class Calendar {
       this.simulationDate.setUTCSeconds(updatedSecond);
     }
 
+    if (unit === 'rate') {
+      currentValue = this.propagationRate;
+      const updatedRate = (currentValue + change);
+      this.updatePropRate(updatedRate)
+    }
+
     this.datetimeInputFormChange();
-    this.updateTimeInput();
+    if (unit != 'rate') {
+      this.updateTimeInput();
+    }
     this.render();
     this.attachEvents();
   }
@@ -226,8 +247,8 @@ export class Calendar {
 
   private renderDayHeaders(): string {
     const daysOfWeek = [
-      i18next.t('time.days-short.0'), i18next.t('time.days-short.1'), i18next.t('time.days-short.2'),
-      i18next.t('time.days-short.3'), i18next.t('time.days-short.4'), i18next.t('time.days-short.5'), i18next.t('time.days-short.6'),
+      i18next.t('time.days-short.1'), i18next.t('time.days-short.2'), i18next.t('time.days-short.3'),
+      i18next.t('time.days-short.4'), i18next.t('time.days-short.5'), i18next.t('time.days-short.6'), i18next.t('time.days-short.0')
     ];
 
 
@@ -240,7 +261,7 @@ export class Calendar {
 
   private renderCalendarDays(): string {
     const firstDay = new Date(this.calendarDate.getUTCFullYear(), this.calendarDate.getUTCMonth(), 1).getUTCDay();
-    const daysInMonth = new Date(this.calendarDate.getUTCFullYear(), this.calendarDate.getUTCMonth() + 1, 0).getUTCDate();
+    const daysInMonth = new Date(this.calendarDate.getUTCFullYear(), this.calendarDate.getUTCMonth() + 1, 0).getUTCDate() + 1;
     const today = new Date();
     let dayHtml = '';
     let dayCount = 1;
@@ -265,7 +286,7 @@ export class Calendar {
             isSelected ? 'ui-datepicker-current-day' : '',
           ].filter(Boolean).join(' ');
 
-          const jday = this.getUTCDayOfYear(new Date(this.calendarDate.getUTCFullYear(), this.calendarDate.getUTCMonth(), dayCount));
+          const jday = this.getUTCDayOfYear(new Date(this.calendarDate.getUTCFullYear(), this.calendarDate.getUTCMonth(), dayCount)) + 1;
 
           const dayCountPadded = dayCount.toString().padStart(2, '0');
           const jdayPadded = jday.toString().padStart(3, '0');
@@ -304,13 +325,15 @@ export class Calendar {
     const mn = doy.getUTCMonth();
     const dn = doy.getUTCDate();
     const dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    let dayInYear = 365;
     let dayOfYear = dayCount[mn] + dn;
 
     if (mn > 1 && this.isLeapYear(doy)) {
       dayOfYear++;
+      dayInYear++;
     }
 
-    return dayOfYear;
+    return dayOfYear % dayInYear;
   }
 
   private getUTCDateFromDayOfYear(year: number, dayOfYear: number): Date {
@@ -336,13 +359,14 @@ export class Calendar {
     const hours = this.simulationDate.getUTCHours();
     const minutes = this.simulationDate.getUTCMinutes();
     const seconds = this.simulationDate.getUTCSeconds();
+    const propRate = this.propagationRate;
 
     return `
       <div class="ui-timepicker-div">
         <dl>
           <dt class="ui_tpicker_time_label">${i18next.t('time.calendar.time')}</dt>
           <dd class="ui_tpicker_time">
-            <input id="calendar-time-input" class="ui_tpicker_time_input" value="${this.formatTime(hours, minutes, seconds)}" disabled>
+            <input id="calendar-time-input" class="ui_tpicker_time_input" value="${this.formatTime(hours, minutes, seconds)} | (x${propRate})" disabled>
           </dd>
           <dt class="ui_tpicker_hour_label">${i18next.t('time.calendar.hour')}</dt>
           <dd class="ui_tpicker_hour">
@@ -387,6 +411,22 @@ export class Calendar {
                 <span class="ui-button-icon-space"> </span>-
               </button>
               <button data-icon="ui-icon-plus" data-step="1" class="ui-widget ui-button-icon-only ui-controlgroup-item ui-button ui-corner-right" title="+" id="second_increase">
+                <span class="ui-button-icon ui-icon ui-icon-plus"></span>
+                <span class="ui-button-icon-space"> </span>+
+              </button>
+            </span>
+          </dd>
+          <dt class="ui_tpicker_proprate_label">${i18next.t('Propagation')}</dt>
+          <dd class="ui_tpicker_proprate">
+            <div id="ui_tpicker_proprate_slider" class="ui_tpicker_proprate_slider ui-slider ui-corner-all ui-slider-horizontal ui-widget ui-widget-content" style="display: inline-block; width: 100px;" data-min="-100" data-max="100" data-step="1">
+              <span tabindex="0" class="ui-slider-handle ui-corner-all ui-state-default" style="left: ${((propRate + 100) / 200) * 100}%;"></span>
+            </div>
+            <span class="ui-slider-access ui-controlgroup ui-controlgroup-horizontal ui-helper-clearfix ui-corner-left" role="toolbar" style="margin-left: 10px; margin-right: 0px;">
+              <button data-icon="ui-icon-minus" data-step="-1" class="ui-widget ui-button-icon-only ui-controlgroup-item ui-button ui-corner-left" title="-" id="rate_decrease">
+                <span class="ui-button-icon ui-icon ui-icon-minus"></span>
+                <span class="ui-button-icon-space"> </span>-
+              </button>
+              <button data-icon="ui-icon-plus" data-step="1" class="ui-widget ui-button-icon-only ui-controlgroup-item ui-button ui-corner-right" title="+" id="rate_increase">
                 <span class="ui-button-icon ui-icon ui-icon-plus"></span>
                 <span class="ui-button-icon-space"> </span>+
               </button>
@@ -464,7 +504,20 @@ export class Calendar {
   private setToNow(): void {
     this.calendarDate = new Date();
     this.simulationDate = new Date(this.calendarDate);
+    this.updatePropRate(1)
     this.datetimeInputFormChange();
+    this.render();
+    this.attachEvents();
+  }
+
+  private pauseProp(): void {
+    this.updatePropRate(0)
+    this.render();
+    this.attachEvents();
+  }
+
+  private playProp(): void {
+    this.updatePropRate(1)
     this.render();
     this.attachEvents();
   }
@@ -508,6 +561,15 @@ export class Calendar {
     }
   }
 
+  private updatePropRate(propRate: number): void {
+    const timeManagerInstance = keepTrackApi.getTimeManager();
+    timeManagerInstance.calculateSimulationTime();
+    timeManagerInstance.changePropRate(propRate)
+    settingsManager.isPropRateChange = true;
+    this.updateSliderPosition('ui_tpicker_proprate_slider', propRate, 200, -100);
+    this.propagationRate = propRate
+  }
+
   private updateHour(hour: number): void {
     this.simulationDate.setUTCHours(hour);
     this.datetimeInputFormChange();
@@ -529,14 +591,14 @@ export class Calendar {
     this.updateSliderPosition('ui_tpicker_second_slider', second, 59);
   }
 
-  private updateSliderPosition(sliderId: string, value: number, max: number): void {
+  private updateSliderPosition(sliderId: string, value: number, range: number, min: number = 0): void {
     const slider = document.getElementById(sliderId);
 
     if (slider) {
       const handle = slider.querySelector('.ui-slider-handle') as HTMLElement;
 
       if (handle) {
-        handle.style.left = `${(value / max) * 100}%`;
+        handle.style.left = `${((value - min) / range) * 100}%`;
       }
     }
   }

a83defedc5cc1ad11d7ee0feb4c321799782db40
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-04-27 00:45:14 -0400

    fix: :bug: fix satellite view not being enabled

--- src/plugins/satellite-view/satellite-view.ts
+++ src/plugins/satellite-view/satellite-view.ts
@@ -19,13 +19,14 @@
  * /////////////////////////////////////////////////////////////////////////////
  */
 
-import { MenuMode, ToastMsgType } from '@app/interfaces';
+import { KeepTrackApiEvents, MenuMode, ToastMsgType } from '@app/interfaces';
 import { keepTrackApi } from '@app/keepTrackApi';
 import { getEl } from '@app/lib/get-el';
 import { shake } from '@app/lib/shake';
 import { t7e } from '@app/locales/keys';
 import { CameraType } from '@app/singletons/camera';
 import viewInAirPng from '@public/img/icons/view-in-air.png';
+import { DetailedSatellite } from 'ootk';
 import { KeepTrackPlugin } from '../KeepTrackPlugin';
 import { SelectSatManager } from '../select-sat-manager/select-sat-manager';
 
@@ -44,16 +45,34 @@ export class SatelliteViewPlugin extends KeepTrackPlugin {
   isRequireSatelliteSelected = true;
   bottomIconImg = viewInAirPng;
   isIconDisabledOnLoad = true;
+
+  addJs(): void {
+    super.addJs();
+
+    keepTrackApi.register({
+      event: KeepTrackApiEvents.selectSatData,
+      cbName: this.id,
+      cb: (obj) => {
+        if (obj instanceof DetailedSatellite) {
+          this.setBottomIconToEnabled();
+        } else {
+          this.setBottomIconToDisabled();
+        }
+      },
+    });
+  }
+
+
   bottomIconCallback = () => {
     if (keepTrackApi.getMainCamera().cameraType === CameraType.SATELLITE) {
       const uiManagerInstance = keepTrackApi.getUiManager();
 
       uiManagerInstance.hideSideMenus();
       keepTrackApi.getMainCamera().cameraType = CameraType.FIXED_TO_SAT; // Back to normal Camera Mode
-      getEl(this.bottomIconElementName).classList.remove('bmenu-item-selected');
+      getEl(this.bottomIconElementName)!.classList.remove('bmenu-item-selected');
     } else if (this.selectSatManager_.selectedSat !== -1) {
       keepTrackApi.getMainCamera().cameraType = CameraType.SATELLITE; // Activate Satellite Camera Mode
-      getEl(this.bottomIconElementName).classList.add('bmenu-item-selected');
+      getEl(this.bottomIconElementName)!.classList.add('bmenu-item-selected');
     } else {
       const uiManagerInstance = keepTrackApi.getUiManager();
 

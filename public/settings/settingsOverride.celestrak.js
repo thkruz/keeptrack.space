468ced1e38eec43102eee81cb6f3655e48d5b27b
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-04-28 22:20:34 -0400

    build: :building_construction: use dotenv instead of multiple branches for celestrak version

---
+++ public/settings/settingsOverride.celestrak.js
@@ -0,0 +1,162 @@
+/**
+ * /////////////////////////////////////////////////////////////////////////////
+ *
+ * @Copyright (C) 2025 Kruczek Labs LLC
+ *
+ * KeepTrack is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * KeepTrack is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * KeepTrack. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * /////////////////////////////////////////////////////////////////////////////
+ */
+
+// Settings Manager Overrides
+const settingsOverride = {
+  /*
+   * This is an example of available settings that can be overridden.
+   * Uncomment any options you wish to change.
+   *
+   */
+  plugins: {
+    debug: false,
+    satInfoboxCore: true,
+    aboutManager: false,
+    collisions: true,
+    trackingImpactPredict: true,
+    dops: false,
+    findSat: true,
+    launchCalendar: false,
+    newLaunch: false,
+    nextLaunch: false,
+    nightToggle: true,
+    photoManager: true,
+    screenRecorder: true,
+    satChanges: false,
+    stereoMap: true,
+    timeMachine: false,
+    initialOrbit: false,
+    missile: false,
+    breakup: false,
+    editSat: true,
+    constellations: true,
+    countries: true,
+    colorsMenu: true,
+    shortTermFences: true,
+    orbitReferences: true,
+    analysis: true,
+    plotAnalysis: true,
+    sensorFov: true,
+    sensorSurv: true,
+    satelliteFov: true,
+    satelliteView: true,
+    planetarium: true,
+    astronomy: true,
+    screenshot: true,
+    watchlist: true,
+    sensor: true,
+    settingsMenu: true,
+    graphicsMenu: true,
+    datetime: true,
+    social: true,
+    topMenu: true,
+    classificationBar: true,
+    soundManager: true,
+    gamepad: true,
+    debrisScreening: true,
+    videoDirector: true,
+    reports: true,
+    polarPlot: true,
+    timeline: true,
+    timelineAlt: true,
+    transponderChannelData: true,
+    calculator: true,
+    createSat: true,
+    filterMenu: true,
+    RPOCalculator: true,
+  },
+  dataSources: {
+    tle: 'https://api.keeptrack.space/v2/sats/celestrak',
+    externalTLEsOnly: false,
+    tleDebris: 'https://app.keeptrack.space/tle/TLEdebris.json',
+    vimpel: 'https://r2.keeptrack.space/vimpel.json',
+    /** This determines if tle source is loaded to supplement externalTLEs  */
+    isSupplementExternal: false,
+  },
+  isShowSecondaryLogo: true,
+  satShader: {
+    /**
+     * The minimum zoom level at which large objects are displayed.
+     */
+    largeObjectMinZoom: 0.37,
+    /**
+     * The maximum zoom level at which large objects are displayed.
+     */
+    largeObjectMaxZoom: 0.58,
+    /**
+     * The minimum size of objects in the shader.
+     */
+    minSize: 5.5,
+    /**
+     * The minimum size of objects in the shader when in planetarium mode.
+     */
+    minSizePlanetarium: 20.0,
+    /**
+     * The maximum size of objects in the shader when in planetarium mode.
+     */
+    maxSizePlanetarium: 20.0,
+    /**
+     * The maximum allowed size of objects in the shader.
+     * This value is dynamically changed based on zoom level.
+     */
+    maxAllowedSize: 35.0,
+    /**
+     * Whether or not to use dynamic sizing for objects in the shader.
+     */
+    isUseDynamicSizing: false,
+    /**
+     * The scalar value used for dynamic sizing of objects in the shader.
+     */
+    dynamicSizeScalar: 1.0,
+    /**
+     * The size of stars and searched objects in the shader.
+     */
+    starSize: '20.0',
+    /**
+     * The distance at which objects start to grow in kilometers.
+     * Must be a float as a string for the GPU to read.
+     * This makes stars bigger than satellites.
+     */
+    distanceBeforeGrow: '14000.0',
+    /**
+     * The blur radius factor used for satellites.
+     */
+    blurFactor1: '0.76',
+    /**
+     * The blur alpha factor used for satellites.
+     */
+    blurFactor2: '0.4',
+    /**
+     * The blur radius factor used for stars.
+     */
+    blurFactor3: '0.43',
+    /**
+     * The blur alpha factor used for stars.
+     */
+    blurFactor4: '0.25',
+    /**
+     * The maximum size of objects in the shader.
+     */
+    maxSize: 70.0,
+  },
+  isEnableJscCatalog: false,
+};
+
+// Expose these to the console
+window.settingsOverride = settingsOverride;

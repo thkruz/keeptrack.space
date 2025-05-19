256d7954779bfe249bec3c773a03d56e85cf69f5
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-05-19 05:48:23 -0400

    Enhance KeyboardHandler to manage Shift key behavior for uppercase and lowercase letters

--- src/doris/input/keyboard-handler.ts
+++ src/doris/input/keyboard-handler.ts
@@ -70,7 +70,7 @@ export class KeyboardHandler {
       return;
     }
 
-    const key = event.key.toLowerCase();
+    const key = event.key;
     const isRepeat = this.keyStates.get(key) || false;
 
     /*
@@ -91,6 +91,38 @@ export class KeyboardHandler {
       event.ctrlKey,
       event.shiftKey,
     );
+
+    if (key === 'Shift') {
+      // Loop through all uppercase letters and change them to lowercase when the shift key is released
+      for (let i = 97; i <= 122; i++) {
+        const lower = String.fromCharCode(i);
+        const upper = lower.toUpperCase();
+
+        if (this.keyStates.get(lower)) {
+          this.keyStates.set(lower, false);
+          this.keyStates.set(upper, true);
+          this.eventBus.emit(
+            InputEvents.KeyUp,
+            event,
+            lower,
+            event.ctrlKey,
+            event.shiftKey,
+          );
+          this.eventBus.emit(
+            InputEvents.KeyDown,
+            event,
+            upper,
+            false,
+            event.ctrlKey,
+            event.shiftKey,
+          );
+        }
+      }
+    }
+  }
+
+  getKeysState(): Map<string, boolean> {
+    return this.keyStates;
   }
 
   /**
@@ -102,7 +134,7 @@ export class KeyboardHandler {
       return;
     }
 
-    const key = event.key.toLowerCase();
+    const key = event.key;
 
     this.keyStates.set(key, false);
 
@@ -113,6 +145,34 @@ export class KeyboardHandler {
       event.ctrlKey,
       event.shiftKey,
     );
+
+    if (key === 'Shift') {
+      // Loop through all uppercase letters and change them to lowercase when the shift key is released
+      for (let i = 65; i <= 90; i++) {
+        const upper = String.fromCharCode(i);
+        const lower = upper.toLowerCase();
+
+        if (this.keyStates.get(upper)) {
+          this.keyStates.set(upper, false);
+          this.keyStates.set(lower, true);
+          this.eventBus.emit(
+            InputEvents.KeyUp,
+            event,
+            upper,
+            event.ctrlKey,
+            event.shiftKey,
+          );
+          this.eventBus.emit(
+            InputEvents.KeyDown,
+            event,
+            lower,
+            false,
+            event.ctrlKey,
+            event.shiftKey,
+          );
+        }
+      }
+    }
   }
 }
 

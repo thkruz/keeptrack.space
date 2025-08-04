980c9980ad90c77864179e65b42b6ae06c486c12
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-08-04 05:53:22 -0400

    feat: :sparkles: add UserAccountPlugin with initialization error handling

---
+++ src/plugins/user-account/user-account.ts
@@ -0,0 +1,7 @@
+export class UserAccountPlugin {
+  init() {
+    console.error(
+      'User account plugin is a pro plugin. Your .env file is improperly configured or you do not have the pro files!',
+    );
+  }
+}

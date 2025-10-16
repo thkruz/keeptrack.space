b7d2a760cdbdaeb3e5e6e69f07c139992b1e229b
Theodore Kruczek <theodore.kruczek@gmail.com>
2025-10-16 06:17:30 -0400

    feat: :globe_with_meridians: Add localization support for satellite orbital data in multiple languages

--- src/plugins/sat-info-box-orbital/sat-info-box-orbital.ts
+++ src/plugins/sat-info-box-orbital/sat-info-box-orbital.ts
@@ -4,37 +4,16 @@ import { SatMath } from '@app/app/analysis/sat-math';
 import { MissileObject } from '@app/app/data/catalog-manager/MissileObject';
 import { SensorMath } from '@app/app/sensors/sensor-math';
 import { EventBusEvent } from '@app/engine/events/event-bus-events';
+import { html } from '@app/engine/utils/development/formatter';
 import { errorManagerInstance } from '@app/engine/utils/errorManager';
 import { getEl, setInnerHtml } from '@app/engine/utils/get-el';
 import { keepTrackApi } from '@app/keepTrackApi';
+import { t7e } from '@app/locales/keys';
 import { BaseObject, DetailedSatellite, eci2lla, MINUTES_PER_DAY } from 'ootk';
 import { KeepTrackPlugin } from '../../engine/plugins/base-plugin';
 import { SatInfoBox } from '../sat-info-box/sat-info-box';
 import { SelectSatManager } from '../select-sat-manager/select-sat-manager';
-import { html } from '@app/engine/utils/development/formatter';
-
-const SECTIONS = {
-  ORBITAL: 'orbital-section',
-};
-
-const EL = {
-  LATITUDE: 'sat-latitude',
-  LONGITUDE: 'sat-longitude',
-  ALTITUDE: 'sat-altitude',
-  PERIOD: 'sat-period',
-  VELOCITY: 'sat-velocity',
-  INCLINATION: 'sat-inclination',
-  ECCENTRICITY: 'sat-eccentricity',
-  RAAN: 'sat-raan',
-  ARG_PE: 'sat-argPe',
-  MEAN_ANOMALY: 'sat-meanAnomaly',
-  APOGEE: 'sat-apogee',
-  PERIGEE: 'sat-perigee',
-  ELSET_AGE: 'sat-elset-age',
-  UNCERTAINTY_RADIAL: 'sat-uncertainty-radial',
-  UNCERTAINTY_INTRACK: 'sat-uncertainty-intrack',
-  UNCERTAINTY_CROSSTRACK: 'sat-uncertainty-crosstrack',
-};
+import { EL, SECTIONS } from './sat-info-box-orbital-html';
 
 export class SatInfoBoxOrbital extends KeepTrackPlugin {
   readonly id = 'SatInfoBoxOrbital';
@@ -42,6 +21,10 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
 
   // Starting values of the collapsable sections
   private readonly isOrbitalSectionCollapsed_ = true;
+
+  init(): void {
+    super.init();
+  }
 
   addHtml(): void {
     super.addHtml();
@@ -68,7 +51,7 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
     return html`
         <div id="${SECTIONS.ORBITAL}">
           <div class="sat-info-section-header">
-            Orbit Data
+            <span>${t7e('SatInfoBoxOrbital.title')}</span>
             <span id="${SECTIONS.ORBITAL}-collapse" class="section-collapse material-icons">expand_less</span>
           </div>
           <!-- Orbital data rows -->
@@ -80,22 +63,30 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
   private createOrbitalDataRows(): string {
     // Create all orbital data rows
     const rows = [
-      { key: 'Apogee', id: EL.APOGEE, tooltip: 'Highest Point in the Orbit', value: 'xxx km' },
-      { key: 'Perigee', id: EL.PERIGEE, tooltip: 'Lowest Point in the Orbit', value: 'xxx km' },
-      { key: 'Inclination', id: EL.INCLINATION, tooltip: 'Angle Measured from Equator on the Ascending Node', value: 'xxx.xx' },
-      { key: 'Eccentricity', id: EL.ECCENTRICITY, tooltip: 'How Circular the Orbit Is (0 is a Circle)', value: 'x.xx' },
-      { key: 'Right Asc.', id: EL.RAAN, tooltip: 'Where it Rises Above the Equator', value: 'x.xx' },
-      { key: 'Arg of Perigee', id: EL.ARG_PE, tooltip: 'Where the Lowest Part of the Orbit Is', value: 'x.xx' },
-      { key: 'Latitude', id: EL.LATITUDE, tooltip: 'Current Latitude Over Earth', value: 'x.xx' },
-      { key: 'Longitude', id: EL.LONGITUDE, tooltip: 'Current Longitude Over Earth', value: 'x.xx' },
-      { key: 'Altitude', id: EL.ALTITUDE, tooltip: 'Current Altitude Above Sea Level', value: 'xxx km' },
-      { key: 'Period', id: EL.PERIOD, tooltip: 'Time for One Complete Revolution Around Earth', value: 'xxx min' },
-      { key: 'Velocity', id: EL.VELOCITY, tooltip: 'Current Velocity of the Satellite (Higher the Closer to Earth it Is)', value: 'xxx km/s' },
-      { key: 'Age of GP', id: EL.ELSET_AGE, tooltip: 'Time Since Official Orbit Calculated (Older GPs are Less Accuarate Usually)', value: 'xxx.xxxx' },
-      { key: 'Radial Sigma', id: EL.UNCERTAINTY_RADIAL, tooltip: 'Radial Uncertainty (meters)', value: 'xxx.xxxx' },
-      { key: 'In Track Sigma', id: EL.UNCERTAINTY_INTRACK, tooltip: 'In Track Uncertainty (meters)', value: 'xxx.xxxx' },
-      { key: 'Cross Track Sigma', id: EL.UNCERTAINTY_CROSSTRACK, tooltip: 'Cross Track Uncertainty (meters)', value: 'xxx.xxxx' },
+      { key: t7e('SatInfoBoxOrbital.Apogee.label'), id: EL.APOGEE, tooltip: t7e('SatInfoBoxOrbital.Apogee.tooltip'), value: 'xxx km' },
+      { key: t7e('SatInfoBoxOrbital.Perigee.label'), id: EL.PERIGEE, tooltip: t7e('SatInfoBoxOrbital.Perigee.tooltip'), value: 'xxx km' },
+      { key: t7e('SatInfoBoxOrbital.Inclination.label'), id: EL.INCLINATION, tooltip: t7e('SatInfoBoxOrbital.Inclination.tooltip'), value: 'xxx.xx' },
+      { key: t7e('SatInfoBoxOrbital.Eccentricity.label'), id: EL.ECCENTRICITY, tooltip: t7e('SatInfoBoxOrbital.Eccentricity.tooltip'), value: 'x.xx' },
+      { key: t7e('SatInfoBoxOrbital.RightAsc.label'), id: EL.RAAN, tooltip: t7e('SatInfoBoxOrbital.RightAsc.tooltip'), value: 'x.xx' },
+      { key: t7e('SatInfoBoxOrbital.ArgOfPerigee.label'), id: EL.ARG_PE, tooltip: t7e('SatInfoBoxOrbital.ArgOfPerigee.tooltip'), value: 'x.xx' },
+      { key: t7e('SatInfoBoxOrbital.Latitude.label'), id: EL.LATITUDE, tooltip: t7e('SatInfoBoxOrbital.Latitude.tooltip'), value: 'x.xx' },
+      { key: t7e('SatInfoBoxOrbital.Longitude.label'), id: EL.LONGITUDE, tooltip: t7e('SatInfoBoxOrbital.Longitude.tooltip'), value: 'x.xx' },
+      { key: t7e('SatInfoBoxOrbital.Altitude.label'), id: EL.ALTITUDE, tooltip: t7e('SatInfoBoxOrbital.Altitude.tooltip'), value: 'xxx km' },
+      { key: t7e('SatInfoBoxOrbital.Period.label'), id: EL.PERIOD, tooltip: t7e('SatInfoBoxOrbital.Period.tooltip'), value: 'xxx min' },
+      { key: t7e('SatInfoBoxOrbital.Velocity.label'), id: EL.VELOCITY, tooltip: t7e('SatInfoBoxOrbital.Velocity.tooltip'), value: 'xxx km/s' },
+      { key: t7e('SatInfoBoxOrbital.AgeOfGP.label'), id: EL.ELSET_AGE, tooltip: t7e('SatInfoBoxOrbital.AgeOfGP.tooltip'), value: 'xxx.xxxx' },
+      { key: t7e('SatInfoBoxOrbital.RadialSigma.label'), id: EL.UNCERTAINTY_RADIAL, tooltip: t7e('SatInfoBoxOrbital.RadialSigma.tooltip'), value: 'xxx.xxxx' },
+      { key: t7e('SatInfoBoxOrbital.InTrackSigma.label'), id: EL.UNCERTAINTY_INTRACK, tooltip: t7e('SatInfoBoxOrbital.InTrackSigma.tooltip'), value: 'xxx.xxxx' },
+      { key: t7e('SatInfoBoxOrbital.CrossTrackSigma.label'), id: EL.UNCERTAINTY_CROSSTRACK, tooltip: t7e('SatInfoBoxOrbital.CrossTrackSigma.tooltip'), value: 'xxx.xxxx' },
     ];
+
+    if (!settingsManager.plugins.SatInfoBoxOrbital!.isShowCovariance) {
+      /*
+       * Remove the covariance rows (Radial Sigma, In Track Sigma, Cross Track Sigma) if the setting is disabled
+       * These rows are at indices 12, 13, and 14 in the 'rows' array
+       */
+      rows.splice(12, 3);
+    }
 
     return rows.map((row) => html`
         <div class="sat-info-row sat-only-info">
@@ -111,8 +102,8 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
     }
 
     if (obj instanceof DetailedSatellite) {
-      setInnerHtml(EL.APOGEE, `${obj.apogee.toFixed(0)} km`);
-      setInnerHtml(EL.PERIGEE, `${obj.perigee.toFixed(0)} km`);
+      setInnerHtml(EL.APOGEE, `${obj.apogee.toFixed(0)} ${t7e('SatInfoBoxOrbital.kilometer')}`);
+      setInnerHtml(EL.PERIGEE, `${obj.perigee.toFixed(0)} ${t7e('SatInfoBoxOrbital.kilometer')}`);
       setInnerHtml(EL.INCLINATION, `${obj.inclination.toFixed(2)}°`);
       setInnerHtml(EL.ECCENTRICITY, obj.eccentricity.toFixed(3));
       setInnerHtml(EL.RAAN, `${obj.rightAscension.toFixed(2)}°`);
@@ -120,65 +111,72 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
 
       const periodDom = getEl(EL.PERIOD);
 
-      if(periodDom) {
-        periodDom.innerHTML = `${obj.period.toFixed(2)} min`;
+      if (periodDom) {
+        periodDom.innerHTML = `${obj.period.toFixed(2)} ${t7e('SatInfoBoxOrbital.Period.min')}`;
         periodDom.dataset.position = 'top';
         periodDom.dataset.delay = '50';
-        periodDom.dataset.tooltip = `Mean Motion: ${(MINUTES_PER_DAY / obj.period).toFixed(2)}`;
+        periodDom.setAttribute(
+          'kt-tooltip',
+          `${t7e('SatInfoBoxOrbital.Period.meanMotion')}: ${(MINUTES_PER_DAY / obj.period).toFixed(2)} ${t7e('SatInfoBoxOrbital.Period.revPerDay')}`,
+        );
       }
 
       const now: Date | number | string = new Date();
       const daysold = obj.ageOfElset(now);
       const age = daysold >= 1 ? daysold : daysold * 24;
-      const units = daysold >= 1 ? 'Days' : 'Hours';
+      const units = daysold >= 1 ? t7e('SatInfoBoxOrbital.AgeOfGP.days') : t7e('SatInfoBoxOrbital.AgeOfGP.hours');
 
       const elsetAgeDom = getEl(EL.ELSET_AGE);
 
-      if(elsetAgeDom) {
+      if (elsetAgeDom) {
         elsetAgeDom.innerHTML = `${age.toFixed(2)} ${units}`;
         elsetAgeDom.dataset.position = 'top';
         elsetAgeDom.dataset.delay = '50';
-        elsetAgeDom.dataset.tooltip = `Epoch Year: ${obj.tle1.substr(18, 2).toString()} Day: ${obj.tle1.substr(20, 8).toString()}`;
+        elsetAgeDom.setAttribute(
+          'kt-tooltip',
+          `${t7e('SatInfoBoxOrbital.AgeOfGP.epochYear')}: ${obj.tle1.substring(18, 20)}<br/>
+           ${t7e('SatInfoBoxOrbital.AgeOfGP.epochDay')}: ${obj.tle1.substring(20, 28)}`,
+        );
       }
 
       const gmst = keepTrackApi.getTimeManager().gmst;
       const lla = eci2lla(obj.position, gmst);
 
-      const satLonElement = getEl('sat-longitude');
-      const satLatElement = getEl('sat-latitude');
+      const satLonElement = getEl(EL.LONGITUDE);
+      const satLatElement = getEl(EL.LATITUDE);
 
       if (satLonElement && satLatElement) {
         if (lla.lon >= 0) {
-          satLonElement.innerHTML = `${lla.lon.toFixed(3)}°E`;
+          satLonElement.innerHTML = `${lla.lon.toFixed(3)}°${t7e('SatInfoBoxOrbital.Longitude.east')}`;
         } else {
-          satLonElement.innerHTML = `${(lla.lon * -1).toFixed(3)}°W`;
+          satLonElement.innerHTML = `${(lla.lon * -1).toFixed(3)}°${t7e('SatInfoBoxOrbital.Longitude.west')}`;
         }
         if (lla.lat >= 0) {
-          satLatElement.innerHTML = `${lla.lat.toFixed(3)}°N`;
+          satLatElement.innerHTML = `${lla.lat.toFixed(3)}°${t7e('SatInfoBoxOrbital.Latitude.north')}`;
         } else {
-          satLatElement.innerHTML = `${(lla.lat * -1).toFixed(3)}°S`;
+          satLatElement.innerHTML = `${(lla.lat * -1).toFixed(3)}°${t7e('SatInfoBoxOrbital.Latitude.south')}`;
         }
       }
 
     }
-    const satAltitudeElement = getEl('sat-altitude');
-    const satVelocityElement = getEl('sat-velocity');
+    const satAltitudeElement = getEl(EL.ALTITUDE);
+    const satVelocityElement = getEl(EL.VELOCITY);
 
     if (satAltitudeElement && satVelocityElement) {
 
       if (obj instanceof DetailedSatellite) {
         const gmst = keepTrackApi.getTimeManager().gmst;
 
-        satAltitudeElement.innerHTML = `${SatMath.getAlt(obj.position, gmst).toFixed(2)} km`;
-        satVelocityElement.innerHTML = `${obj.totalVelocity.toFixed(2)} km/s`;
+        satAltitudeElement.innerHTML = `${SatMath.getAlt(obj.position, gmst).toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}`;
+        satVelocityElement.innerHTML = `${obj.totalVelocity.toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}/${t7e('SatInfoBoxOrbital.second')}`;
       } else {
         const misl = obj as MissileObject;
 
-        satAltitudeElement.innerHTML = `${(keepTrackApi.getSensorManager().currentTEARR?.alt ?? 0).toFixed(2)} km`;
+        satAltitudeElement.innerHTML = `${(keepTrackApi.getSensorManager().currentTEARR?.alt ?? 0).toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}`;
         if (misl.totalVelocity) {
-          satVelocityElement.innerHTML = `${misl.totalVelocity.toFixed(2)} km/s`;
+          satVelocityElement.innerHTML = `${misl.totalVelocity.toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}/${t7e('SatInfoBoxOrbital.second')}`;
         } else {
-          satVelocityElement.innerHTML = 'Unknown';
+          satVelocityElement.innerHTML = t7e('SatInfoBoxOrbital.unknown');
         }
       }
     }
@@ -196,21 +194,21 @@ export class SatInfoBoxOrbital extends KeepTrackPlugin {
         covInTrack > 0.5;
 
       if (useKm) {
-        setInnerHtml('sat-uncertainty-radial', `${(covMatrix[0]).toFixed(2)} km`);
-        setInnerHtml('sat-uncertainty-crosstrack', `${(covMatrix[1]).toFixed(2)} km`);
-        setInnerHtml('sat-uncertainty-intrack', `${(covMatrix[2]).toFixed(2)} km`);
+        setInnerHtml(EL.UNCERTAINTY_RADIAL, `${(covMatrix[0]).toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}`);
+        setInnerHtml(EL.UNCERTAINTY_CROSSTRACK, `${(covMatrix[1]).toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}`);
+        setInnerHtml(EL.UNCERTAINTY_INTRACK, `${(covMatrix[2]).toFixed(2)} ${t7e('SatInfoBoxOrbital.kilometer')}`);
       } else {
         covRadial *= 1000;
         covCrossTrack *= 1000;
         covInTrack *= 1000;
-        setInnerHtml('sat-uncertainty-radial', `${covRadial.toFixed(2)} m`);
-        setInnerHtml('sat-uncertainty-crosstrack', `${covCrossTrack.toFixed(2)} m`);
-        setInnerHtml('sat-uncertainty-intrack', `${covInTrack.toFixed(2)} m`);
+        setInnerHtml(EL.UNCERTAINTY_RADIAL, `${covRadial.toFixed(2)} ${t7e('SatInfoBoxOrbital.meter')}`);
+        setInnerHtml(EL.UNCERTAINTY_CROSSTRACK, `${covCrossTrack.toFixed(2)} ${t7e('SatInfoBoxOrbital.meter')}`);
+        setInnerHtml(EL.UNCERTAINTY_INTRACK, `${covInTrack.toFixed(2)} ${t7e('SatInfoBoxOrbital.meter')}`);
       }
     } else {
-      setInnerHtml('sat-uncertainty-radial', 'Unknown');
-      setInnerHtml('sat-uncertainty-crosstrack', 'Unknown');
-      setInnerHtml('sat-uncertainty-intrack', 'Unknown');
+      setInnerHtml(EL.UNCERTAINTY_RADIAL, t7e('SatInfoBoxOrbital.unknown'));
+      setInnerHtml(EL.UNCERTAINTY_CROSSTRACK, t7e('SatInfoBoxOrbital.unknown'));
+      setInnerHtml(EL.UNCERTAINTY_INTRACK, t7e('SatInfoBoxOrbital.unknown'));
     }
 
     const secondarySatObj = keepTrackApi.getPlugin(SelectSatManager)!.secondarySatObj;

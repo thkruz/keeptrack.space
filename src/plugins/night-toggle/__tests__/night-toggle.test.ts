0e86b4c4384cc15a5195c5cc58cec27503c1bff4
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-05-30 11:55:35 -0400

    test(toggles): :white_check_mark: cover night/sound/hide-other-sats plugin methods to 100% lines

--- src/plugins/night-toggle/__tests__/night-toggle.test.ts
+++ src/plugins/night-toggle/__tests__/night-toggle.test.ts
@@ -15,3 +15,51 @@ describe('NightToggle', () => {
   standardPluginSuite(NightToggle, 'NightToggle');
   standardPluginMenuButtonTests(NightToggle, 'Night_Toggle');
 });
+
+describe('NightToggle methods', () => {
+  let plugin: NightToggle;
+
+  beforeEach(() => {
+    setupStandardEnvironment();
+    plugin = new NightToggle();
+    plugin.init();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('exposes config, "N" shortcut and command', () => {
+    expect(plugin.getBottomIconConfig().elementName).toBe('night-toggle-bottom-icon');
+    expect(plugin.getKeyboardShortcuts()[0].key).toBe('N');
+    expect(() => plugin.getKeyboardShortcuts()[0].callback()).not.toThrow();
+    expect(plugin.getCommandPaletteCommands()[0].id).toBe('NightToggle.toggle');
+    expect(() => plugin.getCommandPaletteCommands()[0].callback()).not.toThrow();
+  });
+
+  it('turns night mode on via toggleNightMode/onBottomIconClick when active', () => {
+    plugin.isMenuButtonActive = true;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawNightAsDay).toBe(true);
+  });
+
+  it('turns night mode off when inactive', () => {
+    plugin.isMenuButtonActive = false;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawNightAsDay).toBe(false);
+  });
+
+  it('on() and off() set the setting directly', () => {
+    plugin.on();
+    expect(settingsManager.isDrawNightAsDay).toBe(true);
+    plugin.off();
+    expect(settingsManager.isDrawNightAsDay).toBe(false);
+  });
+
+  it('bridges bottomIconCallback to onBottomIconClick', () => {
+    const spy = vi.spyOn(plugin, 'onBottomIconClick');
+
+    plugin.bottomIconCallback();
+    expect(spy).toHaveBeenCalled();
+  });
+});

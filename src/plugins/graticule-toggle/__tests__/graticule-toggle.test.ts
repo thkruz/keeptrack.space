a5022038668ec675ae9d5583ae409ea1b34ca1cd
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-05-30 11:51:48 -0400

    test(toggles): :white_check_mark: cover political-map, graticule, skip-interpolation toggle methods to 100% lines

--- src/plugins/graticule-toggle/__tests__/graticule-toggle.test.ts
+++ src/plugins/graticule-toggle/__tests__/graticule-toggle.test.ts
@@ -1,7 +1,7 @@
-import { vi } from 'vitest';
 import { GraticuleToggle } from '@app/plugins/graticule-toggle/graticule-toggle';
 import { setupStandardEnvironment } from '@test/environment/standard-env';
 import { standardPluginSuite } from '@test/generic-tests';
+import { vi } from 'vitest';
 
 describe('GraticuleToggle', () => {
   beforeEach(() => {
@@ -13,4 +13,43 @@ describe('GraticuleToggle', () => {
   });
 
   standardPluginSuite(GraticuleToggle, 'GraticuleToggle');
+});
+
+describe('GraticuleToggle methods', () => {
+  let plugin: GraticuleToggle;
+
+  beforeEach(() => {
+    setupStandardEnvironment();
+    plugin = new GraticuleToggle();
+    plugin.init();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('exposes its bottom-icon config and "G" shortcut and command', () => {
+    expect(plugin.getBottomIconConfig().elementName).toBe('graticule-toggle-bottom-icon');
+    expect(plugin.getKeyboardShortcuts()[0].key).toBe('G');
+    expect(() => plugin.getKeyboardShortcuts()[0].callback()).not.toThrow();
+    expect(plugin.getCommandPaletteCommands()[0].id).toBe('GraticuleToggle.toggle');
+    expect(() => plugin.getCommandPaletteCommands()[0].callback()).not.toThrow();
+  });
+
+  it('toggles the graticule setting on and off', () => {
+    plugin.isMenuButtonActive = true;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawGraticule).toBe(true);
+
+    plugin.isMenuButtonActive = false;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawGraticule).toBe(false);
+  });
+
+  it('bridges bottomIconCallback to onBottomIconClick', () => {
+    const spy = vi.spyOn(plugin, 'onBottomIconClick');
+
+    plugin.bottomIconCallback();
+    expect(spy).toHaveBeenCalled();
+  });
 });
--- src/plugins/political-map-toggle/__tests__/political-map-toggle.test.ts
+++ src/plugins/political-map-toggle/__tests__/political-map-toggle.test.ts
@@ -1,7 +1,9 @@
-import { vi } from 'vitest';
+import { EventBus } from '@app/engine/events/event-bus';
+import { EventBusEvent } from '@app/engine/events/event-bus-events';
 import { PoliticalMapToggle } from '@app/plugins/political-map-toggle/political-map-toggle';
 import { setupStandardEnvironment } from '@test/environment/standard-env';
 import { standardPluginSuite } from '@test/generic-tests';
+import { vi } from 'vitest';
 
 describe('PoliticalMapToggle', () => {
   beforeEach(() => {
@@ -13,4 +15,49 @@ describe('PoliticalMapToggle', () => {
   });
 
   standardPluginSuite(PoliticalMapToggle, 'PoliticalMapToggle');
+});
+
+describe('PoliticalMapToggle methods', () => {
+  let plugin: PoliticalMapToggle;
+
+  beforeEach(() => {
+    setupStandardEnvironment();
+    plugin = new PoliticalMapToggle();
+    plugin.init();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('exposes its bottom-icon config and "l" shortcut and command', () => {
+    expect(plugin.getBottomIconConfig().elementName).toBe('political-map-toggle-bottom-icon');
+    expect(plugin.getKeyboardShortcuts()[0].key).toBe('l');
+    expect(() => plugin.getKeyboardShortcuts()[0].callback()).not.toThrow();
+    expect(plugin.getCommandPaletteCommands()[0].id).toBe('PoliticalMapToggle.toggle');
+    expect(() => plugin.getCommandPaletteCommands()[0].callback()).not.toThrow();
+  });
+
+  it('toggles the political map setting on and off', () => {
+    plugin.isMenuButtonActive = true;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawPoliticalMap).toBe(true);
+
+    plugin.isMenuButtonActive = false;
+    plugin.onBottomIconClick();
+    expect(settingsManager.isDrawPoliticalMap).toBe(false);
+  });
+
+  it('bridges bottomIconCallback and syncs on uiManagerFinal', () => {
+    const clickSpy = vi.spyOn(plugin, 'onBottomIconClick');
+
+    plugin.bottomIconCallback();
+    expect(clickSpy).toHaveBeenCalled();
+
+    settingsManager.isDrawPoliticalMap = true;
+    const selectSpy = vi.spyOn(plugin, 'setBottomIconToSelected');
+
+    EventBus.getInstance().emit(EventBusEvent.uiManagerFinal);
+    expect(selectSpy).toHaveBeenCalled();
+  });
 });

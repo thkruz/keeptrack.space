2cbd20e5a22655319c93b6168706d35d6b5f9d64
Theodore Kruczek <theodore.kruczek@gmail.com>
2026-06-26 07:13:40 -0600

    refactor(ui-manager): :recycle: prefer dataset over getAttribute

--- src/app/ui/ui-manager.ts
+++ src/app/ui/ui-manager.ts
@@ -384,8 +384,8 @@ export class UiManager {
   private sortBottomIcons() {
     const bottomIcons = document.querySelectorAll('#bottom-icons > div');
     const sortedIcons = Array.from(bottomIcons).sort((a, b) => {
-      const aOrder = parseInt(a.getAttribute('data-order') ?? KeepTrackPlugin.MAX_BOTTOM_ICON_ORDER.toString(), 10);
-      const bOrder = parseInt(b.getAttribute('data-order') ?? KeepTrackPlugin.MAX_BOTTOM_ICON_ORDER.toString(), 10);
+      const aOrder = parseInt((a as HTMLElement).dataset.order ?? KeepTrackPlugin.MAX_BOTTOM_ICON_ORDER.toString(), 10);
+      const bOrder = parseInt((b as HTMLElement).dataset.order ?? KeepTrackPlugin.MAX_BOTTOM_ICON_ORDER.toString(), 10);
 
       return aOrder - bOrder;
     });
@@ -410,8 +410,8 @@ export class UiManager {
 
       const icons = Array.from(container.querySelectorAll(':scope > div'));
       const sorted = icons.sort((a, b) => {
-        const aOrder = parseInt(a.getAttribute('data-order') ?? '100', 10);
-        const bOrder = parseInt(b.getAttribute('data-order') ?? '100', 10);
+        const aOrder = parseInt((a as HTMLElement).dataset.order ?? '100', 10);
+        const bOrder = parseInt((b as HTMLElement).dataset.order ?? '100', 10);
 
         return aOrder - bOrder;
       });
